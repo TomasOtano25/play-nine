cfc1c88218c0becab78988f542599bc4e163606c
TomasOtano25 <35155721+TomasOtano25@users.noreply.github.com>
2018-03-05 16:25:07 -0400

    deploy

--- src/index.js
+++ src/index.js
@@ -7,15 +7,16 @@ const Stars = props => {
   // Obtener un numero random del 1 al 9
   const numberOfStars = 1 + Math.floor(Math.random() * 9);
 
-  let stars = [];
+  /*let stars = [];
 
   for (let i = 0; i < numberOfStars; i++) {
     stars.push(<i key={i} className="fas fa-star" />);
-  }
+  }*/
 
   return (
     <div className="col-5">
-      {stars}
+      {_.range(numberOfStars).map(i => <i key={i} className="fas fa-star" />)}
+      {/*{stars}*/}
       {/*<i className="fas fa-star" />
       <i className="fas fa-star" />
       <i className="fas fa-star" />
@@ -33,15 +34,20 @@ const Button = props => {
 };
 
 const Answer = props => {
-  return <div className="col-5">...</div>;
+  return (
+    <div className="col-5">
+      <span>5</span>
+      <span>6</span>
+    </div>
+  );
 };
 
 const Numbers = props => {
-  let arrayOfNumbers = _.range(1, 9);
+  let arrayOfNumbers = _.range(1, 10);
   return (
     <div className="card text-center">
       <div>
-        {arrayOfNumbers.map(numbers => <span key={numbers}>{numbers}</span>)}
+        {arrayOfNumbers.map((number, i) => <span key={i}>{number}</span>)}
         {/*<span>1</span>
         <span className="selected">2</span>
         <span className="used">3</span>*/}
